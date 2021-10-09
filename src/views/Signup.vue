7981af3010c74e1f8bbbc5442e95de11720054a9
MSterpin <msterpin@student.unipu.hr>
2021-10-09 10:27:18 +0200

    Fipugram basic layout

---
+++ src/views/Signup.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="about">
+    <h1>This is an signup page</h1>
+    <div class="container">
+      <div class="row">
+        <div class="col-sm"></div>
+        <div class="col-sm">
+          <form>
+            <div class="form-group">
+              <label for="exampleInputPassword1">Username</label>
+              <input
+                type="username"
+                class="form-control"
+                id="exampleInputUsername1"
+                placeholder="Username"
+              />
+            </div>
+            <div class="form-group">
+              <label for="exampleInputEmail1">Email address</label>
+              <input
+                type="email"
+                class="form-control"
+                id="exampleInputEmail1"
+                aria-describedby="emailHelp"
+                placeholder="Enter email"
+              />
+              <small id="emailHelp" class="form-text text-muted"
+                >We'll never share your email with anyone else.</small
+              >
+            </div>
+            <div class="form-group">
+              <label for="exampleInputPassword1">Password</label>
+              <input
+                type="password"
+                class="form-control"
+                id="exampleInputPassword1"
+                placeholder="Password"
+              />
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+          </form>
+        </div>
+        <div class="col-sm"></div>
+      </div>
+    </div>
+  </div>
+</template>
