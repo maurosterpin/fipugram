6bd00c1472f360e0de55c077d1a1a9fae7117bc6
MSterpin <92162018+maurosterpin@users.noreply.github.com>
2021-10-09 20:16:05 +0200

    Fipugram basic layout upgrade

--- src/components/FipugramCard.vue
+++ src/components/FipugramCard.vue
@@ -1,15 +1,43 @@
 <template>
-  <div class="card text-center">
-    <div class="card-header">
-      Featured
+  <div class="card">
+    <div class="card-header bg-transparent">
+      <img src="@/assets/44.jpg" width="35" class="rounded-circle" />
+      <strong class="ml-3">username</strong>
     </div>
     <div class="card-body p-0"></div>
     <img src="https://picsum.photos/800" />
-    <div class="card-img-top card-footer text-muted">
+    <div class="card-img-top card-footer text-muted bg-transparent">
       2 days ago
     </div>
-  </div></template
->
+    <div class="input-group input-group-lg">
+      <input
+        type="text"
+        class="form-control"
+        placeholder="Add a comment..."
+        aria-label="Comment"
+        aria-describedby="button-addon2"
+      />
+      <div class="input-group-append bg-transparent">
+        <button
+          class="btn bg-transparent border text-muted"
+          type="button"
+          id="button-addon2"
+          style="font-weight: 600; font-size: 15px;"
+        >
+          Post
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+::-webkit-input-placeholder {
+  font-size: 15px !important;
+  font-weight: 600;
+  color: gray;
+}
+</style>
 
 <script>
 export default {
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,9 +4,7 @@
     <div class="col-7">
       <fipugram-card />
     </div>
-    <div class="col-3">
-      ... ovdje ide desni stupac!
-    </div>
+    <div class="col-3">... ovdje ide desni stupac! {{ hello }}</div>
   </div>
 </template>
 
@@ -14,8 +12,15 @@
 // @ is an alias to /src
 import FipugramCard from "@/components/FipugramCard.vue";
 
+let hello = "world";
+
 export default {
   name: "Home",
+  data: function() {
+    return {
+      hello: hello,
+    };
+  },
   components: {
     FipugramCard,
   },
